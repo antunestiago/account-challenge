e0e5c23a4102496ef25b6f2f538a76583fc83894
Tiago Antunes <tiago_antunes14@gmail.com>
2022-01-11 17:46:48 -0400

    Success case done

--- transaction-history/index.js
+++ transaction-history/index.js
@@ -1,18 +1,25 @@
 const account = require('../account/index');
 const transaction = require('../transaction/index');
+const writeOutput = require('../writer-output')
 
 let transactions_history = [];
 
 exports.history = (payload) => {
 
     if(!account.clientExists(payload['document'])) {
-        writeOutput.writeOutputPayload({"type": "transaction", "status": "failure", "violation": "account_not_initialized"});
+        writeOutput.writeOutputPayload({"type": "transaction_history", "status": "failure", "violation": "account_not_initialized"});
         return new Error("Transaction Failed.");;
     }   
 
-    console.log(transactions_history.filter(th => th['account'] === payload.document));
+    client_history = transactions_history.filter(th => th['account'] === payload.document);
 
-    //writeOutput.writeOutputPayload({"type": "transaction", "status": "success", "result": result});
+    client_history.map(ch => {
+        const {account, ...restProperties} = ch; //remove account key
+        return restProperties;
+    });
+    
+    writeOutput.writeOutputPayload({"type": "transaction_history", "status": "success", "result": client_history});
+    
     return {"status": "Ok"};
 }  
 
